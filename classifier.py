955479d984d2f7a5e5ba4515425b24559609d87c
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2024-08-19 14:46:36 +0200

    Update classifier.py
    Removed outdated comments

--- classifier.py
+++ classifier.py
@@ -4,21 +4,11 @@ from data import class_type, data_train, features
 # Allowed imports: Modules included in Python (see https://docs.python.org/3.10/py-modindex.html),
 # and external libraries listed in requirements.txt
 
-
-# Helper function for showing data (not part of assignment)
-def print_data():
-    """ Print animal name and feature names and values """
-    for animal_name, animal_features in data_train.items():
-        feature_string = ', '.join([f'{feature_name} = {feature_value}'
-                                    for feature_name, feature_value in zip(features, animal_features)])
-        print(f'{animal_name}: ' + feature_string)
-
-
 # Modify the code below to return "sensible" classes based on input features.
-# Feel free to add and remove code, the silly if statements below are only
-# meant to show how features can be used and combined.
-# The code returns the placeholder integer 0 in every case. The final classifier
-# must return integers in the range 1-7 (corresponding to 7 animal classes)
+# The final classifier must return integers in the range 1-7 (corresponding to 
+# 7 animal classes). Don't expect to get to 100% correct classification. 
+# It's most important that your code makes sense, has at least some correct
+# classifications, and that you document your thinking/method in the report. 
 def classify_animal(hair, feathers, eggs, milk, airborne, aquatic, predator, toothed,
                     backbone, breathes, venomous, fins, legs, tail, domestic, catsize):
     """ Classifies animal based on 16 features 
@@ -32,9 +22,18 @@ def classify_animal(hair, feathers, eggs, milk, airborne, aquatic, predator, too
     ---------
     class_int: int
         Integer in range 1-7 corresponding to 7 classes of animal
-
     """
     pass
+
+
+# Helper function for showing data (not part of assignment)
+def print_data():
+    """ Print animal name and feature names and values """
+    for animal_name, animal_features in data_train.items():
+        feature_string = ', '.join([f'{feature_name} = {feature_value}'
+                                    for feature_name, feature_value 
+                                    in zip(features, animal_features)])
+        print(f'{animal_name}: ' + feature_string)
 
 
 def run_classifier():
